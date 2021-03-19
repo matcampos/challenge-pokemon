8078e57f17dff275dab8d15f3bf00ee59dc75cdf
matcampos <matheus.alves1998@hotmail.com>
2021-03-19 16:58:24 -0300

    Adding scroll position restore on routeModule in appRoutingModule

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,7 +14,12 @@ const routes: Routes = [
 ];
 
 @NgModule({
-    imports: [RouterModule.forRoot(routes)],
+    imports: [RouterModule.forRoot(routes, {
+        anchorScrolling: "disabled",
+        onSameUrlNavigation: "reload",
+        scrollPositionRestoration: 'enabled',
+        initialNavigation: 'enabled'
+    })],
     exports: [RouterModule]
 })
 export class AppRoutingModule { }
