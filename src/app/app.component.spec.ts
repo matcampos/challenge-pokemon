bf497e1f80212f9a0d6b52e9deeaae59ea928318
matcampos <matheus.alves1998@hotmail.com>
2021-03-18 23:50:12 -0300

    Finishing internationalization

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,12 +1,14 @@
 import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { TranslateModule } from '@ngx-translate/core';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
     beforeEach(async(() => {
         TestBed.configureTestingModule({
             imports: [
-                RouterTestingModule
+                RouterTestingModule,
+                TranslateModule.forRoot()
             ],
             declarations: [
                 AppComponent
